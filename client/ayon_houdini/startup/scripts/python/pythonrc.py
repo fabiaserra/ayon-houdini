ad81902f850f2ceb1551ba77be7f47038aead76e
farrizabalaga <farrizabalaga@alkemy-x.com>
2024-08-05 18:33:45 +0200

    Update to use a single pythonrc file

--- client/ayon_houdini/startup/scripts/python/pythonrc.py
+++ client/ayon_houdini/startup/scripts/python/pythonrc.py
@@ -1,2 +1,14 @@
+# -*- coding: utf-8 -*-
+"""AYON startup script."""
+from ayon_core.pipeline import install_host
+from ayon_houdini.api import HoudiniHost
 from ayon_houdini.nodes import decorator
-decorator.init()
+
+
+def main():
+    print("Installing AYON ...")
+    install_host(HoudiniHost())
+    decorator.init()
+
+
+main()
